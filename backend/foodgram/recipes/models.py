bb53bbb7a18d935d5b9fdf7de2a80984bc0af353
Sergey Trunov <sergeytru15@gmail.com>
2023-03-15 12:55:51 +0300

    Rename models

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -5,23 +5,23 @@ from django.db import models
 User = get_user_model()
 
 
-class Tags(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     color = models.CharField(max_length=7, unique=True)
     slug = models.SlugField(unique=True)
 
 
-class Ingredients(models.Model):
+class Ingredient(models.Model):
     name = models.CharField(max_length=50)
     amount = models.IntegerField()
     measurement_unit = models.CharField(max_length=20)
 
 
-class Recipes(models.Model):
+class Recipe(models.Model):
     author = models.ForeignKey(User, related_name='recipes', on_delete=models.CASCADE)
     name = models.CharField(max_length=50)
     image = models.ImageField(upload_to='recipes/images/', null=True, default=None)
     text = models.TextField()
-    ingredients = models.ManyToManyField(Ingredients)
-    tags = models.ManyToManyField(Tags)
+    ingredients = models.ManyToManyField(Ingredient)
+    tags = models.ManyToManyField(Tag)
     cooking_time = models.IntegerField()
