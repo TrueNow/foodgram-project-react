14ce7fd51968ae9c027994f210ad17202084f16d
Sergey Trunov <sergeytru15@gmail.com>
2023-03-15 15:05:51 +0300

    created viewset and serializer for Tag's urls

--- backend/foodgram/api/urls.py
+++ backend/foodgram/api/urls.py
@@ -7,7 +7,7 @@ app_name = 'api'
 router_v1 = DefaultRouter()
 # router_v1.register('users', views.UserViewSet, basename='users')  # == GET/POST ... {id} == GET ... {me} == GET
 # router_v1.register(r'users/(?P<user_id>\d+)/subscribe', views.SubscribesViewSet, basename='subscribes')  # POST/DELETE
-# router_v1.register('tags', views.TagViewSet, basename='tags')  # == GET ... {id} == GET
+router_v1.register('tags', views.TagViewSet, basename='tags')  # == GET ... {id} == GET
 # router_v1.register('ingredient', views.IngredientViewSet, basename='tags')  # == GET ... {id} == GET
 # router_v1.register('recipes', views.RecipeViewSet, basename='recipes')  # ALL
 # router_v1.register(r'recipes/(?P<recipe_id>\d+)/shopping_cart', views.ShoppingsViewSet, basename='shopping_cart')  # POST/DELETE
@@ -30,8 +30,8 @@ recipes_urlpatterns = [
 
 
 urlpatterns = [
-    path('users', include(users_urlpatterns, namespace='users')),
-    path('auth/token', include(auth_urlpatterns, namespace='auth')),
-    path('recipes', include(recipes_urlpatterns, namespace='recipes')),
+    path('users', include((users_urlpatterns, 'users'), namespace='users')),
+    path('auth/token', include((auth_urlpatterns, 'users'), namespace='auth')),
+    path('recipes', include((recipes_urlpatterns, 'recipes'), namespace='recipes')),
     path('', include(router_v1.urls)),
 ]
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from recipes.models import Tag
+from rest_framework import viewsets
 
-# Create your views here.
+from . import serializers
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = serializers.TagSerializer
