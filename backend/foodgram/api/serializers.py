04402bf11b0db891bfb06d8f4c1814146e99df6c
Sergey Trunov <sergeytru15@gmail.com>
2023-03-15 16:14:09 +0300

    created RAW viewset and serializers for Users's urls

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,5 +1,26 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from recipes.models import Tag, Ingredient
+
+
+User = get_user_model()
+
+
+class UserReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'email', 'id', 'username', 'first_name', 'last_name',
+            # 'is_subscribed'
+        )
+
+
+class UserWriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'email', 'username', 'first_name', 'last_name', 'password'
+        )
 
 
 class TagSerializer(serializers.ModelSerializer):
--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -1,7 +1,29 @@
+from django.contrib.auth import get_user_model
 from recipes.models import Tag, Ingredient
-from rest_framework import viewsets
+from rest_framework import viewsets, decorators, response, mixins
 
 from . import serializers
+
+
+User = get_user_model()
+
+
+class UserViewSet(mixins.CreateModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.ListModelMixin,
+                  viewsets.GenericViewSet):
+    queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve', 'me'):
+            return serializers.UserReadSerializer
+        return serializers.UserWriteSerializer
+
+    @decorators.action(methods=['get'], detail=False, url_path='me', url_name='me')
+    def me(self, request):
+        instance = self.request.user
+        serializer = self.get_serializer(instance)
+        return response.Response(serializer.data)
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
