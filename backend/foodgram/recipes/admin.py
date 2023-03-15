45286bf87b3d0f828a3e5548573595ee201fcc44
Sergey Trunov <sergeytru15@gmail.com>
2023-03-15 21:03:34 +0300

    register all models in admin

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, Tag
+from .models import Recipe, Ingredient, Tag, Favorite, ShoppingCart
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(Tag)
+admin.site.register(Favorite)
+admin.site.register(ShoppingCart)
