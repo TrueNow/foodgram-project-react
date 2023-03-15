22bfba240c8ce3a01cdfdf17d223ca3050adf447
Sergey Trunov <sergeytru15@gmail.com>
2023-03-15 13:40:35 +0300

    Create model User

--- backend/foodgram/users/models.py
+++ backend/foodgram/users/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    first_name = models.CharField('Имя', max_length=150, blank=False)
+    last_name = models.CharField('Фамилия', max_length=150, blank=False)
+    email = models.EmailField('Email', blank=False)
+
+
+class Subscription(models.Model):
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name='subscriber')
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='following')
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = models.UniqueConstraint(
+            fields=['user', 'author'],
+            name='unique_following'
+        )
+
+    def __str__(self):
+        return f'{self.subscriber} подписан на {self.author}'
