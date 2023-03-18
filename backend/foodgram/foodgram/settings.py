4563aa5e0bcb4c1ec7bcd96f7629b2db0492792e
Sergey Trunov <sergeytru15@gmail.com>
2023-03-18 11:01:50 +0300

    startapp core

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
     'recipes.apps.RecipesConfig',
+    'core.apps.CoreConfig',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
