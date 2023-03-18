8591727f19c845dc1a53f5900387ab406942fff1
Sergey Trunov <sergeytru15@gmail.com>
2023-03-18 11:21:12 +0300

    update permissions

--- backend/foodgram/api/permissions.py
+++ backend/foodgram/api/permissions.py
@@ -7,16 +7,13 @@ AllowAny = permissions.AllowAny
 class RecipePermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.action in (
-                'create', 'update', 'partial_update', 'destroy',
-                'favorite', 'shopping_cart', 'download_shopping_cart'
+            'create', 'update', 'partial_update', 'destroy',
+            'favorite', 'shopping_cart', 'download_shopping_cart'
         ):
             return request.user.is_authenticated
         return request.user.is_anonymous
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_anonymous:
-            return False
-
         if view.action in ('update', 'partial_update', 'destroy'):
-            return obj.author == request.user
-        return False
+            return request.user.is_authenticated and obj.author == request.user
+        return request.user.is_anonymous
