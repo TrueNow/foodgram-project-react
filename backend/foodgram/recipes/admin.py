d85e73784cc4bb7ede3399a51fc63d41ba1273c8
Sergey Trunov <sergeytru15@gmail.com>
2023-03-21 23:06:14 +0300

    Create recipes admin.py

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,9 +1,53 @@
 from django.contrib import admin
-from .models import Recipe, Ingredient, Tag, Favorite, ShoppingCart, IngredientAmount
+from .models import Favorite, Ingredient, IngredientAmount, Recipe, Tag, ShoppingCart
 
-admin.site.register(Recipe)
-admin.site.register(Ingredient)
-admin.site.register(IngredientAmount)
-admin.site.register(Tag)
-admin.site.register(Favorite)
-admin.site.register(ShoppingCart)
+
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('^name',)
+
+
+class IngredientAmountAdmin(admin.TabularInline):
+    model = IngredientAmount
+    fk_name = 'recipe'
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('author', 'name', 'favorited')
+    list_filter = ('author', 'name', 'tags')
+    exclude = ('ingredients',)
+    search_fields = ('^name',)
+
+    inlines = [
+        IngredientAmountAdmin,
+    ]
+
+    @admin.display(empty_value='Никто')
+    def favorited(self, obj):
+        return Favorite.objects.filter(recipe=obj).count()
+
+    favorited.short_description = 'Кол-во людей добавивших в избранное'
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'color', 'slug')
+
+
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
+
+
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+
+
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+
+
+admin.site.register(Ingredient, IngredientAdmin)
+admin.site.register(IngredientAmount, RecipeIngredientAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(ShoppingCart, ShoppingCartAdmin)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Favorite, FavoriteAdmin)
